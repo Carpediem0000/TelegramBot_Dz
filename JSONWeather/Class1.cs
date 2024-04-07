10c3c0c2e5e89fca8bca536b39fd6120ccf2c189
CarpeDiem0000 <zinovchukdfuz2@gmail.com>
2024-04-07 23:12:03 +0300

    Add project files.

---
+++ JSONWeather/Class1.cs
@@ -0,0 +1,40 @@
+﻿namespace JSONWeather
+{
+    public class WeatherResponse
+    {
+        public Coord coord { get; set; }
+        public Weather[] weather { get; set; }
+        public Main main { get; set; }
+        public Wind wind { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Coord
+    {
+        public float lon { get; set; }
+        public float lat { get; set; }
+    }
+
+    public class Weather
+    {
+        public int id { get; set; }
+        public string main { get; set; }
+        public string description { get; set; }
+        public string icon { get; set; }
+    }
+
+    public class Main
+    {
+        public float temp { get; set; }
+        public int pressure { get; set; }
+        public int humidity { get; set; }
+        public float temp_min { get; set; }
+        public float temp_max { get; set; }
+    }
+
+    public class Wind
+    {
+        public float speed { get; set; }
+        public int deg { get; set; }
+    }
+}
---
+++ TelegramBot_Dz/Program.cs
@@ -0,0 +1,101 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+using System.Net.Http;
+using Newtonsoft.Json;
+using TelegramBot_Dz;
+
+var botClient = new TelegramBotClient("7173090461:AAFqQHmlp2-HA8Tp9trKquYwejs-B4VSI_E");
+const string apiWeather = "bcb395514086595b809ab3007e0d819a";
+
+using CancellationTokenSource cts = new();
+
+// StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
+ReceiverOptions receiverOptions = new()
+{
+    AllowedUpdates = Array.Empty<UpdateType>() // receive all update types except ChatMember related updates
+};
+
+botClient.StartReceiving(
+   updateHandler: HandleUpdateAsync,
+   pollingErrorHandler: HandlePollingErrorAsync,
+   receiverOptions: receiverOptions,
+   cancellationToken: cts.Token
+);
+
+var me = await botClient.GetMeAsync();
+
+Console.WriteLine($"Start listening for @{me.Username}");
+Console.ReadLine();
+
+// Send cancellation request to stop bot
+cts.Cancel();
+
+async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+{
+    // Only process Message updates: https://core.telegram.org/bots/api#message
+    if (update.Message is not { } message)
+        return;
+
+    var chatId = message.Chat.Id;
+
+
+    
+    var keyboard = new ReplyKeyboardMarkup(new[]
+        {
+            KeyboardButton.WithRequestLocation("Узнать погоду"),
+        });
+    if (message.Text == "/start")
+    {
+        await botClient.SendTextMessageAsync(chatId, message.Text, replyMarkup: keyboard);
+    }
+    if (message.Type == Telegram.Bot.Types.Enums.MessageType.Location)
+    {
+        var latitude = message.Location.Latitude;
+        var longitude = message.Location.Longitude;
+
+        var res = await GetWeatherAsync(latitude, longitude);
+        await botClient.SendTextMessageAsync(chatId, res);
+    }
+}
+
+static async Task<string> GetWeatherAsync(double latitude, double longitude)
+{
+    var apiUrl = $"https://api.openweathermap.org/data/2.5/weather?lat={latitude.ToString("f2")}&lon={longitude.ToString("f2")}&lang=ru&exclude=current&appid={apiWeather}&units=metric";
+
+    HttpClient httpClient = new HttpClient();
+    var response = await httpClient.GetAsync(apiUrl);
+
+    if (response.IsSuccessStatusCode)
+    {
+        var responseContent = await response.Content.ReadAsStringAsync();
+        var weatherResponse = JsonConvert.DeserializeObject<WeatherResponse>(responseContent);
+
+        // Извлекаем нужные данные из объекта
+        string res = $"Город: {weatherResponse.name}\n" +
+            $"Температура: {weatherResponse.main.temp} °C\n" +
+            $"Описание погоды: {weatherResponse.weather[0].description}\n" +
+            $"Скорость ветра: {weatherResponse.wind.speed} м/с";
+        return res;
+    }
+    else
+    {
+        return "Не удалось получить информацию о погоде.";
+    }
+}
+
+Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+{
+    var ErrorMessage = exception switch
+    {
+        ApiRequestException apiRequestException
+            => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+        _ => exception.ToString()
+    };
+
+    Console.WriteLine(ErrorMessage);
+    return Task.CompletedTask;
+}
